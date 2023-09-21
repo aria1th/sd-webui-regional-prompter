3da5e935671b40dfa2c2e1c1ffa6016dd5da77a2
AngelBottomless <aria1th@naver.com>
2023-09-21 16:44:51 +0900

    cleanup some code

--- scripts/image_util.py
+++ scripts/image_util.py
@@ -1,8 +1,10 @@
+"""
+Utility functions for image components.
+"""
 import base64
 from io import BytesIO
 import os
 import gzip
-import json
 import re
 import numpy as np
 from PIL import Image
@@ -52,21 +54,21 @@ def decode_data(data:str):
         # try to decode
         try:
             bytes_data = base64.b64decode(data)
-        except:
-            raise ValueError("Could not decode as base64 data")
+        except Exception as exception:
+            raise ValueError("Could not decode as base64 data") from exception
     else:
         # find invalid characters
         invalid_chars = re.findall(r"[^A-Za-z0-9+/=]", data)
         if invalid_chars:
-            raise ValueError("Invalid characters found in data: {}".format(invalid_chars))
-    assert isinstance(bytes_data, bytes), "Expected bytes data, got {}".format(type(bytes_data))
+            raise ValueError(f"Invalid characters found in data: {invalid_chars}")
+    assert isinstance(bytes_data, bytes), f"Expected bytes data, got {type(bytes_data)}"
     # check if data is gzip compressed
     if is_compressed(bytes_data):
         # try to decompress
         try:
             bytes_data = gzip.decompress(bytes_data)
-        except:
-            raise ValueError("Could not decompress as gzip data")
+        except Exception as execption:
+            raise ValueError("Could not decompress as gzip data") from execption
     # try to load as numpy array
     try:
         bytesIO = BytesIO(bytes_data)
@@ -76,13 +78,11 @@ def decode_data(data:str):
         if image.mode == "RGBA":
             image = image.convert("RGB")
         return np.array(image)
-    except:
-        raise ValueError("Could not load as image data")
+    except Exception as exception:
+        raise ValueError("Could not load as image data") from exception
 
 def is_compressed(data:bytes)->bool:
     """
     Check if data is gzip compressed.
     """
     return data[:2] == b'\x1f\x8b'
-
-
